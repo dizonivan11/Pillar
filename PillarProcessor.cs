b78b5d516f6c2b8f1d741aa43aca106f016fae4f
Ivan Dizon <dizonivan11@gmail.com>
2018-04-12 01:11:18 +0800

    First Version

---
+++ PillarProcessor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace Pillar
+{
+    public class PillarProcessor
+    {
+        public static string ROOT = "C:/pillar/sites/";
+
+        public HttpListenerRequest request;
+        public HttpListenerResponse response;
+
+        public PillarProcessor(HttpListenerRequest request, HttpListenerResponse response) {
+            this.request = request;
+            this.response = response;
+        }
+
+        public void Start() {
+            new Thread(delegate () {
+                try {
+                    string requestUrl = request.RawUrl.Split('?')[0];
+                    if (requestUrl[requestUrl.Length - 1] == '/') requestUrl += "index.phtml";
+
+                    string mediaType = "phtml";
+                    if (requestUrl.IndexOf('.') > -1) mediaType = requestUrl.Split('.')[1].ToLower();
+                    else {
+                        requestUrl += ".phtml";
+                        mediaType = "phtml";
+                    }
+
+                    string method = request.HttpMethod.ToUpperInvariant();
+                    Console.WriteLine("{0}: {1}", method, requestUrl);
+
+                    NameValueCollection POST = new NameValueCollection();
+                    if (method == "POST") {
+                        StreamReader body = new StreamReader(request.InputStream, request.ContentEncoding);
+                        string postContent = body.ReadToEnd();
+
+                        if (postContent.IndexOf('&') > -1) {
+                            string[] keyvalues = postContent.Split('&');
+
+                            for (int i = 0; i < keyvalues.Length; i++) {
+                                if (keyvalues[i].IndexOf('=') > -1) {
+                                    string[] keyvalue = keyvalues[i].Split(new string[] { "=" }, StringSplitOptions.None);
+                                    POST.Add(keyvalue[0], keyvalue[1]);
+                                }
+                            }
+                        } else if (postContent.IndexOf('=') > -1) {
+                            string[] keyvalue = postContent.Split(new string[] { "=" }, StringSplitOptions.None);
+                            POST.Add(keyvalue[0], keyvalue[1]);
+                        }
+                    }
+                    NameValueCollection GET = request.QueryString;
+                    
+                    foreach (string key in POST.Keys) Console.WriteLine("POST-DATA: {0}={1}", key, POST[key]);
+                    foreach (string key in GET.Keys) Console.WriteLine("GET-DATA: {0}={1}", key, GET[key]);
+                    
+                    byte[] buffer = new byte[0];
+                    switch (mediaType) {
+                        case "phtml":
+                            try {
+                                string phtml = File.ReadAllText(ROOT + requestUrl);
+                                buffer = request.ContentEncoding.GetBytes(phtml.ToHtml(POST, GET));
+                            } catch { buffer = request.ContentEncoding.GetBytes("Page not Found"); }
+                            break;
+                        default:
+                            try {
+                                buffer = File.ReadAllBytes(ROOT + requestUrl);
+                            } catch { buffer = new byte[0]; }
+                            break;
+                    }
+
+                    response.ContentLength64 = buffer.Length;
+                    response.OutputStream.Write(buffer, 0, buffer.Length);
+                    response.OutputStream.Flush();
+                } catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+                }
+            }).Start();
+        }
+    }
+}
---
+++ View.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CSharp;
+using CSScriptLibrary;
+using System.Collections.Specialized;
+using System.Net;
+using System.Web;
+
+namespace Pillar {
+    public static class View {
+        // Contains extensions for string
+        const string CSSTART = "<csharp>";
+        const string CSEND = "</csharp>";
+
+        public static string ToHtml(this string phtml, NameValueCollection POST, NameValueCollection GET) {
+            // normalize html variable first before returning it.
+            // This variable contains internal CSharp code (using special tags) which must be converted to html.
+            string html = phtml;
+            
+            int startTag = html.IndexOf(CSSTART);
+            while (startTag > -1) {
+                int endTag = html.IndexOf(CSEND);
+                string csTag = html.Substring(startTag, endTag - startTag + CSEND.Length);
+                string csCode = csTag.Substring(CSSTART.Length, csTag.Length - CSSTART.Length - CSEND.Length);
+
+                CSharpCodeProvider cscp = new CSharpCodeProvider();
+                string format =
+                    "using System;" +
+                    "using System.Collections.Generic;" +
+                    "using System.Text;" +
+                    // css_include <filename>
+                    "" +
+                    "public class Script {{" +
+                    "   StringBuilder returnValue = new StringBuilder();" +
+                    "   DataManager POST = new DataManager();" +
+                    "   DataManager GET = new DataManager();" +
+                    "   VariableManager VAR = new VariableManager();" +
+                    "" +
+                    "   public Script() {{" +
+                    "       {0}" +
+                    "   }}" +
+                    "" +
+                    "   public void Echo(string format, params object[] args) {{" +
+                    "       returnValue.Append(string.Format(format, args));" +
+                    "   }}" +
+                    "" +
+                    "   public string Main() {{" +
+                    "       try {{" +
+                    "           {1}" +
+                    "       }} catch (Exception ex) {{" +
+                    "           Echo(\"<b style='color: red;'>RUNTIME ERROR: \" + ex.Message + \"</b>\");" +
+                    "       }}" +
+                    "       return returnValue.ToString();" +
+                    "   }}" +
+                    "}}" +
+                    "" +
+                    "public class DataManager : Dictionary<string, string> {{" +
+                    "   public string this[string key] {{" +
+                    "       get {{" +
+                    "           if (!ContainsKey(key)) Add(key, string.Empty);" +
+                    "           return base[key];" +
+                    "       }}" +
+                    "   }}" +
+                    "}}" +
+                    "" +
+                    "public class VariableManager : Dictionary<string, object> {{" +
+                    "   public object this[string key] {{" +
+                    "       get {{" +
+                    "           if (!ContainsKey(key)) Add(key, null);" +
+                    "           return base[key];" +
+                    "       }}" +
+                    "       set {{" +
+                    "           if (!ContainsKey(key)) Add(key, value);" +
+                    "           else base[key] = value;" +
+                    "       }}" +
+                    "   }}" +
+                    "}}";
+
+                try {
+                    StringBuilder data = new StringBuilder();
+                    foreach (string key in POST.Keys)
+                        data.AppendFormat("     POST.Add(\"{0}\", \"{1}\");\n",
+                            HttpUtility.UrlDecode(key),
+                            HttpUtility.UrlDecode(POST[key]));
+                    foreach (string key in GET.Keys)
+                        data.AppendFormat("     GET.Add(\"{0}\", \"{1}\");\n",
+                            HttpUtility.UrlDecode(key),
+                            HttpUtility.UrlDecode(GET[key]));
+                    dynamic script = CSScript.Evaluator.LoadCode(string.Format(format, data, csCode));
+                    string returnValue = script.Main();
+                    html = html.Replace(csTag, returnValue);
+                }
+                catch (Exception ex) {
+                    string errorHtml = string.Empty;
+                    errorHtml += "<!DOCTYPE html>";
+                    errorHtml += "<html>";
+                    errorHtml += "    <head>";
+                    errorHtml += "        <title>COMPILE ERROR</title>";
+                    errorHtml += "    </head>";
+                    errorHtml += "    <body>";
+                    errorHtml += "        <h1>COMPILE ERROR</h1>";
+                    errorHtml += "        <h3>" + ex.Message + "</h3>";
+                    errorHtml += "    </body>";
+                    errorHtml += "</html>";
+                    return errorHtml;
+                }
+
+                startTag = html.IndexOf(CSSTART);
+            }
+
+            return html;
+        }
+    }
+}
